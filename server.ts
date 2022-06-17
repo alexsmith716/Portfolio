eb93981495aabed44735cc1deb9b1881808f2220
AlexSmith716 <1691290+alexsmith716@users.noreply.github.com>
2022-06-17 07:51:46 -0400

    refactor: Server and common sc style now TS.

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
-const { createServer } = require('http');
-const { parse } = require('url');
-const next = require('next');
+import { createServer } from 'http';
+import { parse } from 'url';
+import next from 'next';
 
 const dev = process.env.NODE_ENV !== 'production';
 const hostname = 'localhost';
@@ -11,10 +11,10 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
 	createServer((req, res) => {
-		const parsedUrl = parse(req.url, true)
+		const parsedUrl = parse(req.url!, true)
 		handle(req, res, parsedUrl)
-	}).listen(port, (err) => {
-		if (err) throw err
-		console.log(`> Ready on http://${hostname}:${port}`)
-	})
-});
+	}).listen(port)
+
+	// tslint:disable-next-line:no-console
+	console.log(`> Ready on http://${hostname}:${port}`);
+})
